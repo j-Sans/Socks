d5480714fe8ab27d5550c98aac4da77aacc16828
ja-San <jake_sanders@horacemann.org>
2017-02-25 21:56:07 -0500

    Updated main

--- C-Sockets/main.cpp
+++ C-Sockets/main.cpp
@@ -6,11 +6,15 @@
 #include "ClientSocket.hpp"
 
 int main(int argc, const char * argv[]) {
-    int a;
+    /*
+     This is a test which can be used to demonstrate either socket.
+     It can be run as either the ServerSocket or ClientSocket, or by running two instances, as both.
+     */
+    std::cout << "Run as server? Type \'Y\' or \'y\' for yes, and anything else for no." << std::endl;
+    char input;
+    std::cin >> input;
     
-    std::cin >> a;
-    
-    if (a == 0) {
+    if (input == 'Y' || input == 'y') {
         ServerSocket server;
         server.setSocket(3000);
         server.addClient();
@@ -22,6 +26,5 @@ int main(int argc, const char * argv[]) {
         client.send("Hello server!");
         std::cout << "Client received " << client.receive();
     }
-    
     return 0;
 }
