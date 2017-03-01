1e1c09d11a2b5633bbd83b1503799ea2974c06ae
ja-San <jake_sanders@horacemann.org>
2017-02-28 23:40:49 -0500

    Fixed ServerSocket timeout

--- C-Sockets/ServerSocket.cpp
+++ C-Sockets/ServerSocket.cpp
@@ -252,11 +252,17 @@ void ServerSocket::setTimeout(unsigned int seconds, unsigned int milliseconds) {
 #if defined(_WIN32)
     DWORD timeout = (seconds * 1000) + milliseconds;
     setsockopt(this->hostSocket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+    for (int a = 0; a < MAX_NUMBER_OF_CONNECTIONS; a++) {
+        if (this->activeConnections[a]) setsockopt(this->clientSockets[a], SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+    }
 #else
     struct timeval time;
     time.tv_sec = seconds;
     time.tv_usec = (milliseconds * 1000);
-    setsockopt(this->hostSocket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&time, sizeof(time));
+    setsockopt(this->hostSocket, SOL_SOCKET, SO_RCVTIMEO, (struct timeval*)&time, sizeof(struct timeval));
+    for (int a = 0; a < MAX_NUMBER_OF_CONNECTIONS; a++) {
+        if (this->activeConnections[a]) setsockopt(this->clientSockets[a], SOL_SOCKET, SO_RCVTIMEO, (struct timeval*)&time, sizeof(struct timeval));
+    }
 #endif
 }
 
